2ff9905b88830c0a4a6e606750335bc8833b65eb
Aradhya Mishra <odoisveryverygood@gmail.com>
2025-12-01 18:43:19 -0800

    Create PaywallScreen widget with basic layout

---
+++ paywall_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PaywallScreen extends StatelessWidget {
+  const PaywallScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Paywall Screen")),
+    );
+  }
+}

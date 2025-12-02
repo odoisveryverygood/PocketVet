118f5e66135e250594b7f8c1e61f15d3d8eb84c5
Aradhya Mishra <odoisveryverygood@gmail.com>
2025-12-01 18:36:18 -0800

    Create ActivityHistoryScreen widget

---
+++ activity_history_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ActivityHistoryScreen extends StatelessWidget {
+  const ActivityHistoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Activity History Screen")),
+    );
+  }
+}
